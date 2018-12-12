a6a346b344770375672c24c68a962b71a34e9511
Brajesh Jaishwal <brajesh.jaishwal@gmail.com>
2018-12-12 23:51:40 +0530

    for deployment

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c4f8147412650138405c2d186bd39cf4.js"
+  "/precache-manifest.f954e2f37590adbce2b2588c53db70f7.js"
 );
 
 workbox.clientsClaim();
